6bb1a4eaa0aa565cf214e2b3a94c04660ec3ffab
TheWarDoctor95 <tyler@dellajay.com>
2019-03-24 17:28:42 -0400

    Added color property in preparation for adding the ore types, which all have the same itemID and are differentiated by their color

--- glossary/items.py
+++ glossary/items.py
@@ -1,18 +1,20 @@
 class Item:
     name = None
     itemID = None
+    color = None
     category = None
     weight = None
 
-    def __init__ ( self, name, itemID, category, weight ):
+    def __init__ ( self, name, itemID, color, category, weight ):
         self.name = name
         self.itemID = itemID
+        self.color = color
         self.category = category
         self.weight = weight
 
 items = {
     ### Healing ###
-    'bandage': Item( 'bandage', 0x0E21, 'healing', 0.1 ),
+    'bandage': Item( 'bandage', 0x0E21, 0x0000, 'healing', 0.1 ),
     'healingPotion': None,
     'greaterHealingPotion': None,
 
@@ -21,37 +23,46 @@ items = {
 
 
     ### Instruments ###
-    'drum': Item( 'drum', 0x0E9C, 'instrument', None ),
-    'flute': Item( 'flute', 0x2805, 'instrument', None ),
-    'lute': Item( 'lute', 0x0EB3, 'instrument', None ),
+    'drum': Item( 'drum', 0x0E9C, 0x0000, 'instrument', None ),
+    'flute': Item( 'flute', 0x2805, 0x0000, 'instrument', None ),
+    'lute': Item( 'lute', 0x0EB3, 0x0000, 'instrument', None ),
 
     # Harps
-    'harpLap': Item( 'lap harp', 0x0EB2, 'instrument', None ),
-    'harpStanding': Item( 'standing harp', 0x0EB1, 'instrument', None ),
+    'harpLap': Item( 'lap harp', 0x0EB2, 0x0000, 'instrument', None ),
+    'harpStanding': Item( 'standing harp', 0x0EB1, 0x0000, 'instrument', None ),
 
     # Tambourines
-    'tambourine': Item( 'tambourine', 0x0E9E, 'instrument', None ),
-    'tambourineWithTassles': Item( 'tambourineWithRedTassles', 0x0E9D, 'instrument', None ),
+    'tambourine': Item( 'tambourine', 0x0E9E, 0x0000, 'instrument', None ),
+    'tambourineWithTassles': Item( 'tambourineWithRedTassles', 0x0E9D, 0x0000, 'instrument', None ),
 
 
     ### Ores ###
-    'oreIron': Item( 'iron ore', 0x19B9, 'ore', 9 ),
+    'oreIron': Item( 'iron ore', 0x19B9, 0x0000, 'ore', 9 ),
 
 
     ### Reagents ###
-    'Black Pearl': Item( 'Black Pearl', 0x0F7A, 'reagent', 0.1 ),
-    'Blood Moss': Item( 'Blood Moss', 0x0F7B, 'reagent', 0.1 ),
-    'Garlic': Item( 'Garlic', 0x0F84, 'reagent', 0.1 ),
-    'Ginseng': Item( 'Ginseng', 0x0F85, 'reagent', 0.1 ),
-    'Mandrake Root': Item( 'Mandrake Root', 0x0F86, 'reagent', 0.1 ),
-    'Nightshade': Item( 'Nightshade', 0x0F88, 'reagent', 0.1 ),
-    'Spider\'s Silk': Item( 'Spider\'s Silk', 0x0F8D, 'reagent', 0.1 ),
-    'Sulfurous Ash': Item( 'Sulfurous Ash', 0x0F8C, 'reagent', 0.1 )
+    'Black Pearl': Item( 'Black Pearl', 0x0F7A, 0x0000, 'reagent', 0.1 ),
+    'Blood Moss': Item( 'Blood Moss', 0x0F7B, 0x0000, 'reagent', 0.1 ),
+    'Garlic': Item( 'Garlic', 0x0F84, 0x0000, 'reagent', 0.1 ),
+    'Ginseng': Item( 'Ginseng', 0x0F85, 0x0000, 'reagent', 0.1 ),
+    'Mandrake Root': Item( 'Mandrake Root', 0x0F86, 0x0000, 'reagent', 0.1 ),
+    'Nightshade': Item( 'Nightshade', 0x0F88, 0x0000, 'reagent', 0.1 ),
+    'Spider\'s Silk': Item( 'Spider\'s Silk', 0x0F8D, 0x0000, 'reagent', 0.1 ),
+    'Sulfurous Ash': Item( 'Sulfurous Ash', 0x0F8C, 0x0000, 'reagent', 0.1 ),
+
+
+    ### Tools ###
+    'sledge hammer': Item( 'sledge hammer', 0x0FB5, 0x0000, 'tool', 10 ),
+    'smith\'s hammer': Item( 'smith\'s hammer', 0x13E3, 0x0000, 'tool', 8 ),
+    'tinker\'s tools': Item( 'tinker\'s tools', 0x1EBC, 0x0000, 'tool', 1 ),
+    'tongs': Item( 'tongs', 0x0000, 0x0FBB, 'tool', 2 ),
+    'tool kit': Item( 'tool kit', 0x1EB8, 0x0000, 'tool', 1 )
 }
 
 ores = { key: value for key, value in items.items() if value != None and value.category == 'ore' }
 instruments = { key: value for key, value in items.items() if value != None and value.category == 'instrument' }
 reagents = { key: value for key, value in items.items() if value != None and value.category == 'reagent' }
+tools = { key: value for key, value in items.items() if value != None and value.category == 'tool' }
 
 
 def FindItem( itemID, container ):
