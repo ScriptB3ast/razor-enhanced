1cfb392bf7b20eb3fea3447bbdfb3edd1a51d3c2
TheWarDoctor95 <tyler@dellajay.com>
2019-03-24 23:21:01 -0400

    Renamed Item class to myItem so that it doesn't collide with the RE client Item class

--- glossary/items.py
+++ glossary/items.py
@@ -1,4 +1,4 @@
-class Item:
+class myItem:
     name = None
     itemID = None
     color = None
@@ -14,52 +14,52 @@ class Item:
 
 items = {
     ### Healing ###
-    'bandage': Item( 'bandage', 0x0E21, 0x0000, 'healing', 0.1 ),
+    'bandage': myItem( 'bandage', 0x0E21, 0x0000, 'healing', 0.1 ),
     'healingPotion': None,
     'greaterHealingPotion': None,
 
 
     ### Ingots ###
-    'iron ingot': Item( 'iron ingot', 0x1BF2, 0x0000, 'ingot', 0.1 ),
-    'dull copper ingot': Item( 'dull copper ingot', 0x1BF2, 0x0415, 'ingot', 0.1 ),
+    'iron ingot': myItem( 'iron ingot', 0x1BF2, 0x0000, 'ingot', 0.1 ),
+    'dull copper ingot': myItem( 'dull copper ingot', 0x1BF2, 0x0415, 'ingot', 0.1 ),
 
 
     ### Instruments ###
-    'drum': Item( 'drum', 0x0E9C, 0x0000, 'instrument', None ),
-    'flute': Item( 'flute', 0x2805, 0x0000, 'instrument', None ),
-    'lute': Item( 'lute', 0x0EB3, 0x0000, 'instrument', None ),
+    'drum': myItem( 'drum', 0x0E9C, 0x0000, 'instrument', None ),
+    'flute': myItem( 'flute', 0x2805, 0x0000, 'instrument', None ),
+    'lute': myItem( 'lute', 0x0EB3, 0x0000, 'instrument', None ),
 
     # Harps
-    'lap harp': Item( 'lap harp', 0x0EB2, 0x0000, 'instrument', None ),
-    'standing harp': Item( 'standing harp', 0x0EB1, 0x0000, 'instrument', None ),
+    'lap harp': myItem( 'lap harp', 0x0EB2, 0x0000, 'instrument', None ),
+    'standing harp': myItem( 'standing harp', 0x0EB1, 0x0000, 'instrument', None ),
 
     # Tambourines
-    'tambourine': Item( 'tambourine', 0x0E9E, 0x0000, 'instrument', None ),
-    'tambourineWithTassles': Item( 'tambourineWithRedTassles', 0x0E9D, 0x0000, 'instrument', None ),
+    'tambourine': myItem( 'tambourine', 0x0E9E, 0x0000, 'instrument', None ),
+    'tambourineWithTassles': myItem( 'tambourineWithRedTassles', 0x0E9D, 0x0000, 'instrument', None ),
 
 
     ### Ores ###
-    'iron ore': Item( 'iron ore', 0x19B9, 0x0000, 'ore', None ),
-    'dull copper ore': Item( 'dull copper ore', 0x19B9, 0x0415, 'ore', None ),
+    'iron ore': myItem( 'iron ore', 0x19B9, 0x0000, 'ore', None ),
+    'dull copper ore': myItem( 'dull copper ore', 0x19B9, 0x0415, 'ore', None ),
 
 
     ### Reagents ###
-    'Black Pearl': Item( 'Black Pearl', 0x0F7A, 0x0000, 'reagent', 0.1 ),
-    'Blood Moss': Item( 'Blood Moss', 0x0F7B, 0x0000, 'reagent', 0.1 ),
-    'Garlic': Item( 'Garlic', 0x0F84, 0x0000, 'reagent', 0.1 ),
-    'Ginseng': Item( 'Ginseng', 0x0F85, 0x0000, 'reagent', 0.1 ),
-    'Mandrake Root': Item( 'Mandrake Root', 0x0F86, 0x0000, 'reagent', 0.1 ),
-    'Nightshade': Item( 'Nightshade', 0x0F88, 0x0000, 'reagent', 0.1 ),
-    'Spider\'s Silk': Item( 'Spider\'s Silk', 0x0F8D, 0x0000, 'reagent', 0.1 ),
-    'Sulfurous Ash': Item( 'Sulfurous Ash', 0x0F8C, 0x0000, 'reagent', 0.1 ),
+    'Black Pearl': myItem( 'Black Pearl', 0x0F7A, 0x0000, 'reagent', 0.1 ),
+    'Blood Moss': myItem( 'Blood Moss', 0x0F7B, 0x0000, 'reagent', 0.1 ),
+    'Garlic': myItem( 'Garlic', 0x0F84, 0x0000, 'reagent', 0.1 ),
+    'Ginseng': myItem( 'Ginseng', 0x0F85, 0x0000, 'reagent', 0.1 ),
+    'Mandrake Root': myItem( 'Mandrake Root', 0x0F86, 0x0000, 'reagent', 0.1 ),
+    'Nightshade': myItem( 'Nightshade', 0x0F88, 0x0000, 'reagent', 0.1 ),
+    'Spider\'s Silk': myItem( 'Spider\'s Silk', 0x0F8D, 0x0000, 'reagent', 0.1 ),
+    'Sulfurous Ash': myItem( 'Sulfurous Ash', 0x0F8C, 0x0000, 'reagent', 0.1 ),
 
 
     ### Tools ###
-    'sledge hammer': Item( 'sledge hammer', 0x0FB5, 0x0000, 'tool', 10 ),
-    'smith\'s hammer': Item( 'smith\'s hammer', 0x13E3, 0x0000, 'tool', 8 ),
-    'tinker\'s tools': Item( 'tinker\'s tools', 0x1EBC, 0x0000, 'tool', 1 ),
-    'tongs': Item( 'tongs', 0x0000, 0x0FBB, 'tool', 2 ),
-    'tool kit': Item( 'tool kit', 0x1EB8, 0x0000, 'tool', 1 )
+    'sledge hammer': myItem( 'sledge hammer', 0x0FB5, 0x0000, 'tool', 10 ),
+    'smith\'s hammer': myItem( 'smith\'s hammer', 0x13E3, 0x0000, 'tool', 8 ),
+    'tinker\'s tools': myItem( 'tinker\'s tools', 0x1EBC, 0x0000, 'tool', 1 ),
+    'tongs': myItem( 'tongs', 0x0000, 0x0FBB, 'tool', 2 ),
+    'tool kit': myItem( 'tool kit', 0x1EB8, 0x0000, 'tool', 1 )
 }
 
 instruments = { itemName: item for itemName, item in items.items() if item != None and item.category == 'instrument' }
