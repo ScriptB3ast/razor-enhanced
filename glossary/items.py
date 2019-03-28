f311925909a0e96e34fae727bc8dacb64c8247f8
TheWarDoctor95 <tyler@dellajay.com>
2019-03-28 16:28:31 -0400

    Added a function for finding a nearby moongate

--- glossary/items.py
+++ glossary/items.py
@@ -182,3 +182,21 @@ def FindInstrument( container ):
     instrumentIDs = [ instruments[ instrument ].itemID for instrument in instruments ]
 
     return FindItem( instrumentIDs, container )
+
+
+def FindMoongate():
+    '''
+    Finds a moongate that the player can use
+    '''
+    global moongates
+
+    moongateIDs = [ moongates[ moongate ].itemID for moongate in moongates ]
+
+    moongateFilter = Items.Filter()
+    moongateFilter.OnGround = 1
+    moongateFilter.RangeMin = 0
+    moongateFilter.RangeMax = 1
+    moongateFilter.Movable = False
+    moongateFilter.Graphics = List[int]( moongateIDs )
+
+    return Items.ApplyFilter( moongateFilter )
