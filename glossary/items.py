b9554ee1c372c9a1fda3e57f6a7e0018885e891e
TheWarDoctor95 <tyler@dellajay.com>
2019-04-12 08:17:08 -0400

    Added most of the containers

--- glossary/items.py
+++ glossary/items.py
@@ -44,6 +44,20 @@ items = {
     'Kiss the Cook apron': myItem( 'Kiss the Cook apron', 0x153D, 0x0844, 'clothing', 4 ),
 
 
+    ### Containers ###
+    'backpack': myItem( 'backpack', 0x0E75, 0x0000, 'container', 1 ),
+    'bag': myItem( 'bag', 0x0E76, 0x0000, 'container', 1 ),
+    'barrel': myItem( 'barrel', 0x0E77, 0x0000, 'container', 1 ),
+    'crate': myItem( 'crate', 0x0E7E, 0x0000, 'container', 1 ),
+    'metal chest (east/west)': myItem( 'metal chest', 0x0E7C, 0x0000, 'container', 1 ),
+    'metal chest (north/south)': myItem( 'metal chest', 0x2DF3, 0x0000, 'container', 1 ),
+    'ornate elven chest': myItem( 'ornate elven chest', 0x0E79, 0x0000, 'container', 1 ),
+    'pouch': myItem( 'pouch', 0x0E79, 0x0000, 'container', 1 ),
+    'trash barrel': myItem( 'trash barrel', 0x0E77, 0x03B2, 'container', 1 ),
+    'wooden box': myItem( 'wooden box', 0x0E7D, 0x0000, 'container', 1 ),
+    'wooden chest': myItem( 'wooden chest', 0x0E43, 0x0000, 'container', 1 ),
+
+
     ### Healing ###
     'bandage': myItem( 'bandage', 0x0E21, 0x0000, 'healing', 0.1 ),
     'healingPotion': None,
