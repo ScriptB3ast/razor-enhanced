145b81e43c397896dd920ddf2074b192d51ab406
TheWarDoctor95 <tyler@dellajay.com>
2019-04-12 08:57:56 -0400

    Started compiling a list of the tailoring materials

--- glossary/items.py
+++ glossary/items.py
@@ -225,6 +225,13 @@ items = {
     'snow elemental statuette': myItem( 'snow elemental statuette', 0x25DC, 0x0000, 'statuette', 1 ),
 
 
+    ### Tailoring Materials ###
+    'cut cloth': myItem( 'cut cloth', 0x1767, 0x07C2, 'tailoring', 10 ),
+    'pile of folded besotted cloth': myItem( 'pile of folded besotted cloth', 0x1766, 0x0000, 'tailoring', 1 ),
+    'piles of hides': myItem( 'piles of hides', 0x1079, 0x0000, 'tailoring', 10 ),
+    'pieces of leather': myItem( 'pieces of leather', 0x1081, 0x0000, 'tailoring', 1 ),
+
+
     ### Scrolls ###
     # First Circle
     'Clumsy scroll': myItem( 'Clumsy scroll', 0x1F2E, 0x0000, 'magic scroll', 1 ),
