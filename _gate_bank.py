2adf50df2cef99df7b07b0cab52732c8a8c5a6d6
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:15:36 -0400

    Added macro for creating a gate to the bank

---
+++ _gate_bank.py
@@ -0,0 +1,6 @@
+if Player.Name == 'TheWarMage':
+    Gumps.WaitForGump( 1431013363, 2000 )
+    Gumps.SendAction( 1431013363, 0 )
+    Items.UseItem( 0x42783F7F )
+    Gumps.WaitForGump( 1431013363, 2000 )
+    Gumps.SendAction( 1431013363, 24 )
