4d008b652b0c9b6fd97175e1e3a5c549458daef5
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:02:58 -0400

    Moved the tool items into their own file

--- glossary/items.py
+++ glossary/items.py
@@ -16,36 +16,6 @@ class myItem:
         self.category = category
         self.weight = weight
 
-items = {
-    ### Tools ###
-    'disguise kit': myItem( 'disguise kit', 0x0E05, 0x0000, 'tool', None ),
-    'dovetail saw': myItem( 'dovetail saw', 0x1028, 0x0000, 'tool', None ),
-    'dyes': myItem( 'dyes', 0x0FA9, 0x0000, 'tool', None ),
-    'dyeing tub': myItem( 'dyeing tub', 0x0FAB, 0x0000, 'tool', None ),
-    'gargoyle\'s pickaxe': myItem( 'gargoyle\'s pickaxe', 0x0E86, 0x0430, 'tool', 11 ),
-    'interior decorator': myItem( 'interior decorator', 0x0FC1, 0x0000, 'tool', 11 ),
-    'lockpick': myItem( 'lockpick', 0x14FC, 0x0000, 'tool', 11 ),
-    'pickaxe': myItem( 'pickaxe', 0x0E86, 0x0000, 'tool', 11 ),
-    'prospector\'s tool': myItem( 'prospector\'s tool', 0x0FB4, 0x0000, 'tool', 11 ),
-    'sturdy pickaxe': myItem( 'pickaxe', 0x0E86, 0x0973, 'tool', 11 ),
-
-    # Runic Hammers
-    'agapite runic hammer': myItem( 'agapite runic hammer', 0x13E3, 0x097E, 'tool', 8 ),
-    'bronze runic hammer': myItem( 'bronze runic hammer', 0x13E3, 0x06D8, 'tool', 8 ),
-    'copper runic hammer': myItem( 'copper runic hammer', 0x13E3, 0x045F, 'tool', 8 ),
-    'dull copper runic hammer': myItem( 'dull copper runic hammer', 0x13E3, 0x0415, 'tool', 8 ),
-    'golden runic hammer': myItem( 'golden runic hammer', 0x13E3, 0x06B7, 'tool', 8 ),
-    'shadow iron runic hammer': myItem( 'shadow iron runic hammer', 0x13E3, 0x0455, 'tool', 8 ),
-    'verite runic hammer': myItem( 'verite runic hammer', 0x13E3, 0x07D2, 'tool', 8 ),
-    'valorite runic hammer': myItem( 'valorite runic hammer', 0x13E3, 0x0544, 'tool', 8 ),
-
-    'sledge hammer': myItem( 'sledge hammer', 0x0FB5, 0x0000, 'tool', 10 ),
-    'smith\'s hammer': myItem( 'smith\'s hammer', 0x13E3, 0x0000, 'tool', 8 ),
-    'tinker\'s tools': myItem( 'tinker\'s tools', 0x1EBC, 0x0000, 'tool', 1 ),
-    'tongs': myItem( 'tongs', 0x0FBB, 0x0000, 'tool', 2 ),
-    'tool kit': myItem( 'tool kit', 0x1EB8, 0x0000, 'tool', 1 )
-}
-
 instruments = { itemName: item for itemName, item in items.items() if item != None and item.category == 'instrument' }
 moongates = { itemName: item for itemName, item in items.items() if item != None and item.category == 'moongate' }
 ores = { itemName: item for itemName, item in items.items() if item != None and item.category == 'ore' }
---
+++ glossary/items/tools.py
@@ -0,0 +1,294 @@
+from Scripts.utilities.items import myItem, FindItem
+
+tools = {
+    # Blacksmithing
+    'sledge hammer': myItem(
+        name = 'sledge hammer',
+        itemID = 0x0FB5,
+        color = 0x0000,
+        category = 'tool',
+        weight = 10
+    ),
+    'smith\'s hammer': myItem(
+        name = 'smith\'s hammer',
+        itemID = 0x13E3,
+        color = 0x0000,
+        category = 'tool',
+        weight = 8
+    ),
+    'tongs': myItem(
+        name = 'tongs',
+        itemID = 0x0FBB,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+
+    # Runic Hammers
+    'agapite runic hammer': myItem(
+        name = 'agapite runic hammer',
+        itemID = 0x13E3,
+        color = 0x097E,
+        category = 'tool',
+        weight = 8
+    ),
+    'bronze runic hammer': myItem(
+        name = 'bronze runic hammer',
+        itemID = 0x13E3,
+        color = 0x06D8,
+        category = 'tool',
+        weight = 8
+    ),
+    'copper runic hammer': myItem(
+        name = 'copper runic hammer',
+        itemID = 0x13E3,
+        color = 0x045F,
+        category = 'tool',
+        weight = 8
+    ),
+    'dull copper runic hammer': myItem(
+        name = 'dull copper runic hammer',
+        itemID = 0x13E3,
+        color = 0x0415,
+        category = 'tool',
+        weight = 8
+    ),
+    'golden runic hammer': myItem(
+        name = 'golden runic hammer',
+        itemID = 0x13E3,
+        color = 0x06B7,
+        category = 'tool',
+        weight = 8
+    ),
+    'shadow iron runic hammer': myItem(
+        name = 'shadow iron runic hammer',
+        itemID = 0x13E3,
+        color = 0x0455,
+        category = 'tool',
+        weight = 8
+    ),
+    'verite runic hammer': myItem(
+        name = 'verite runic hammer',
+        itemID = 0x13E3,
+        color = 0x07D2,
+        category = 'tool',
+        weight = 8
+    ),
+    'valorite runic hammer': myItem(
+        name = 'valorite runic hammer',
+        itemID = 0x13E3,
+        color = 0x0544,
+        category = 'tool',
+        weight = 8
+    ),
+
+    # Carpentry	Scorp	Smoothing Plane
+    'dovetail saw': myItem(
+        name = 'dovetail saw',
+        itemID = 0x1028,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'draw knife': myItem(
+        name = 'draw knife',
+        itemID = 0x10E4,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'froe': myItem(
+        name = 'froe',
+        itemID = 0x10E5,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'hammer': myItem(
+        name = 'hammer',
+        itemID = 0x102A,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'inshave': myItem(
+        name = 'inshave',
+        itemID = 0x10E6,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'jointing plane': myItem(
+        name = 'jointing plane',
+        itemID = 0x1030,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'moulding planes': myItem(
+        name = 'moulding planes',
+        itemID = 0x102C,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'nails': myItem(
+        name = 'nails',
+        itemID = 0x102E,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'saw': myItem(
+        name = 'saw',
+        itemID = 0x1034,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'scorp': myItem(
+        name = 'scorp',
+        itemID = 0x10E7,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'smoothing plane': myItem(
+        name = 'smoothing plane',
+        itemID = 0x1032,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+
+    # Mining
+    'gargoyle\'s pickaxe': myItem(
+        name = 'gargoyle\'s pickaxe',
+        itemID = 0x0E86,
+        color = 0x0430,
+        category = 'tool',
+        weight = 11
+    ),
+    'pickaxe (left)': myItem(
+        name = 'pickaxe',
+        itemID = 0x0E85,
+        color = 0x0000,
+        category = 'tool',
+        weight = 11
+    ),
+    'pickaxe (right)': myItem(
+        name = 'pickaxe',
+        itemID = 0x0E86,
+        color = 0x0000,
+        category = 'tool',
+        weight = 11
+    ),
+    'prospector\'s tool': myItem(
+        name = 'prospector\'s tool',
+        itemID = 0x0FB4,
+        color = 0x0000,
+        category = 'tool',
+        weight = 11
+    ),
+    'sturdy pickaxe': myItem(
+        name = 'pickaxe',
+        itemID = 0x0E86,
+        color = 0x0973,
+        category = 'tool',
+        weight = 11
+    ),
+
+    # Tinkering
+    'tinker\'s tools': myItem(
+        name = 'tinker\'s tools',
+        itemID = 0x1EBC,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'tool kit': myItem(
+        name = 'tool kit',
+        itemID = 0x1EB8,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+
+    'cleaver': myItem(
+        name = 'cleaver',
+        itemID = 0x0EC3,
+        color = 0x0000,
+        category = 'tool',
+        weight = 2
+    ),
+    'dagger': myItem(
+        name = 'dagger',
+        itemID = 0x0F52,
+        color = 0x0000,
+        category = 'weapon',
+        weight = 1
+    ),
+    'disguise kit': myItem(
+        name = 'disguise kit',
+        itemID = 0x0E05,
+        color = 0x0000,
+        category = 'tool',
+        weight = None
+    ),
+    'dyes': myItem(
+        name = 'dyes',
+        itemID = 0x0FA9,
+        color = 0x0000,
+        category = 'tool',
+        weight = 3
+    ),
+    'dyeing tub': myItem(
+        name = 'dyeing tub',
+        itemID = 0x0FAB,
+        color = 0x0000,
+        category = 'tool',
+        weight = 10
+    ),
+    'fishing pole': myItem(
+        name = 'fishing pole',
+        itemID = 0x0DBF,
+        color = 0x0000,
+        category = 'tool',
+        weight = 8
+    ),
+    'interior decorator': myItem(
+        name = 'interior decorator',
+        itemID = 0x0FC1,
+        color = 0x0000,
+        category = 'tool',
+        weight = 11
+    ),
+    'lockpick': myItem(
+        name = 'lockpick',
+        itemID = 0x14FC,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'mapmaker\'s pen': myItem(
+        name = 'mapmaker\'s pen',
+        itemID = 0x0FBF,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'scissors': myItem(
+        name = 'scissors',
+        itemID = 0x0F9F,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+    'skinning knife': myItem(
+        name = 'skinning knife',
+        itemID = 0x0EC4,
+        color = 0x0000,
+        category = 'tool',
+        weight = 1
+    ),
+}
