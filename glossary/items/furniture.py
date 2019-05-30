dbff485f06a786130343a30e7d0cf954c54b4e09
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:54:19 -0400

    Moved the furniture items into their own file

---
+++ glossary/items/furniture.py
@@ -0,0 +1,14 @@
+from Scripts.utilities.items import items
+
+furniture = {
+    'anvil': myItem( 'anvil', 0x0FB0, 0x0000, 'furniture', None ),
+    'broken chair': myItem( 'broken chair', 0x0C1B, 0x0000, 'furniture', None ),
+    'crystal workbench': myItem( 'crystal workbench', 0x2DF3, 0x0B93, 'furniture', None ),
+    'map storage cabinet': myItem( 'map storage cabinet', 0x285C, 0x0000, 'furniture', None ),
+    'skill scroll storage bookshelf': myItem( 'skill scroll storage bookshelf', 0x0A9A, 0x0501, 'furniture', None ),
+    'stone chair': myItem( 'stone chair', 0x121A, 0x0000, 'furniture', None ),
+    'table': myItem( 'table', 0x0B34, 0x0000, 'furniture', None ),
+    'throne': myItem( 'throne', 0x0B33, 0x0000, 'furniture', None ),
+    'wooden bench (east/west)': myItem( 'wooden bench', 0x0B2C, 0x0000, 'furniture', None ),
+    'wooden bench (north/south)': myItem( 'wooden bench', 0x0B2D, 0x0000, 'furniture', None ),
+}
