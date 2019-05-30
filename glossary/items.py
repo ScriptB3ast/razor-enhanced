cea45a5b06bf1c5a6fc58ebf92241b25811ddde0
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:48:35 -0400

    Moved the furniture items into their own file

--- glossary/items.py
+++ glossary/items.py
@@ -17,19 +17,6 @@ class myItem:
         self.weight = weight
 
 items = {
-    ### Furniture ###
-    'anvil': myItem( 'anvil', 0x0FB0, 0x0000, 'furniture', None ),
-    'broken chair': myItem( 'broken chair', 0x0C1B, 0x0000, 'furniture', None ),
-    'crystal workbench': myItem( 'crystal workbench', 0x2DF3, 0x0B93, 'furniture', None ),
-    'map storage cabinet': myItem( 'map storage cabinet', 0x285C, 0x0000, 'furniture', None ),
-    'skill scroll storage bookshelf': myItem( 'skill scroll storage bookshelf', 0x0A9A, 0x0501, 'furniture', None ),
-    'stone chair': myItem( 'stone chair', 0x121A, 0x0000, 'furniture', None ),
-    'table': myItem( 'table', 0x0B34, 0x0000, 'furniture', None ),
-    'throne': myItem( 'throne', 0x0B33, 0x0000, 'furniture', None ),
-    'wooden bench (east/west)': myItem( 'wooden bench', 0x0B2C, 0x0000, 'furniture', None ),
-    'wooden bench (north/south)': myItem( 'wooden bench', 0x0B2D, 0x0000, 'furniture', None ),
-
-
     ### Generic Items ###
     'ancient smithing hammer shard': myItem( 'ancient smithing hammer shard', 0x5738, 0x09C2, 'generic', 16 ),
     'gold coin': myItem( 'gold coin', 0x0EED, 0x0000, 'generic', 0.02 ),
