588c33272e3215f8da35184ac7c03840b99dc3e9
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:00:06 -0400

    Moved the slayer runes into their own file

---
+++ glossary/items/slayerRunes.py
@@ -0,0 +1,33 @@
+from Scripts.utilities.items import myItem
+
+slayerRunes = {
+    'p rune': myItem( 'p rune', 0x486B, 0x0027, 'slayer rune', 1 ),
+    'b rune': None,
+    'm rune': None,
+    'f rune': myItem( 'f rune', 0x484A, 0x0027, 'slayer rune', 1 ),
+    'v rune': myItem( 'v rune', 0x487D, 0x0027, 'slayer rune', 1 ),
+    'u rune': None,
+
+    't rune': myItem( 't rune', 0x4877, 0x0027, 'slayer rune', 1 ),
+    'd rune': None,
+    'n rune': myItem( 'n rune', 0x4868, 0x0027, 'slayer rune', 1 ),
+    's rune': myItem( 's rune', 0x4874, 0x0027, 'slayer rune', 1 ),
+    'z rune': myItem( 'z rune', 0x4883, 0x0027, 'slayer rune', 1 ),
+    'e rune': myItem( 'e rune', 0x4847, 0x0027, 'slayer rune', 1 ),
+
+    'ch rune': myItem( 'ch rune', 0x4841, 0x0027, 'slayer rune', 1 ),
+    'j rune': myItem( 'j rune', 0x4856, 0x0027, 'slayer rune', 1 ),
+    'sh rune': myItem( 'sh rune', 0x4871, 0x0027, 'slayer rune', 1 ),
+    'zh rune': myItem( 'zh rune', 0x4880, 0x0027, 'slayer rune', 1 ),
+    'i rune': myItem( 'i rune', 0x4853, 0x0027, 'slayer rune', 1 ),
+
+    'gl rune': myItem( 'gl rune', 0x4859, 0x0027, 'slayer rune', 1 ),
+    'l rune': myItem( 'l rune', 0x485F, 0x0027, 'slayer rune', 1 ),
+
+    'k rune': myItem( 'k rune', 0x485C, 0x0027, 'slayer rune', 1 ),
+    'g rune': None,
+    'ng rune': None,
+    'h rune': None,
+    'r rune': myItem( 'r rune', 0x486E, 0x0027, 'slayer rune', 1 ),
+    'a rune': myItem( 'a rune', 0x483B, 0x0027, 'slayer rune', 1 )
+}
