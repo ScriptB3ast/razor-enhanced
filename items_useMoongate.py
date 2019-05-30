00f0eeaf8c3ecb5fa7498d8d3d757eedaa3662d1
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:06:14 -0400

    Modified to leverage the data in glossary/items/moongates.py

--- items_useMoongate.py
+++ items_useMoongate.py
@@ -1,23 +1,8 @@
-from System.Collections.Generic import List
+from Scripts.glossary.items.moongates import FindMoongates
 
-def FindMoongate():
-    '''
-    Finds a moongate that the player can use
-    '''
-    
-    moongateFilter = Items.Filter()
-    moongateFilter.OnGround = 1
-    moongateFilter.RangeMin = 0
-    moongateFilter.RangeMax = 1
-    moongateFilter.Movable = False
-    moongateFilter.Graphics = List[int]( [ 0x0F6C ] )
-    
-    moongate = Items.ApplyFilter( moongateFilter )
-    return moongate
+moongates = FindMoongates( Items )
+if len( moongates ) > 0:
+    Items.UseItem( moongates[ 0 ] )
 
-moongate = FindMoongate()
-if len( moongate ) > 0:
-    Items.UseItem( moongate[ 0 ] )
-    
     Gumps.WaitForGump( 3716879466, 2000 )
     Gumps.SendAction( 3716879466, 1 )
