e9c65862deaf4435ca880d0cca8d80e9dd6506bd
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:01:54 -0400

    Moved the spell scrolls into their own file

---
+++ glossary/items/spellScrolls.py
@@ -0,0 +1,83 @@
+from Scripts.utilities.items import myItem
+
+spellScrolls = {
+    # First Circle
+    'Clumsy scroll': myItem( 'Clumsy scroll', 0x1F2E, 0x0000, 'magic scroll', 1 ),
+    'Create Food scroll': myItem( 'Create Food scroll', 0x1F2F, 0x0000, 'magic scroll', 1 ),
+    'Feeblemind scroll': myItem( 'Feeblemind scroll', 0x1F30, 0x0000, 'magic scroll', 1 ),
+    'Heal scroll': myItem( 'Heal scroll', 0x1F31, 0x0000, 'magic scroll', 1 ),
+    'Magic Arrow scroll': myItem( 'Magic Arrow scroll', 0x1F32, 0x0000, 'magic scroll', 1 ),
+    'Night Sight scroll': myItem( 'Night Sight scroll', 0x1F33, 0x0000, 'magic scroll', 1 ),
+    'Reactive Armor scroll': myItem( 'Reactive Armor scroll', 0x1F2D, 0x0000, 'magic scroll', 1 ),
+    'Weaken scroll': myItem( 'Weaken scroll', 0x1F34, 0x0000, 'magic scroll', 1 ),
+
+    # Second Circle
+    'Agility scroll': myItem( 'Agility scroll', 0x1F35, 0x0000, 'magic scroll', 1 ),
+    'Cunning scroll': myItem( 'Cunning scroll', 0x1F36, 0x0000, 'magic scroll', 1 ),
+    'Cure scroll': myItem( 'Cure scroll', 0x1F37, 0x0000, 'magic scroll', 1 ),
+    'Harm scroll': myItem( 'Harm scroll', 0x1F38, 0x0000, 'magic scroll', 1 ),
+    'Magic Trap scroll': myItem( 'Magic Trap scroll', 0x1F39, 0x0000, 'magic scroll', 1 ),
+    'Magic Untrap scroll': myItem( 'Magic Untrap scroll', 0x1F3A, 0x0000, 'magic scroll', 1 ),
+    'Protection scroll': myItem( 'Protection scroll', 0x1F3B, 0x0000, 'magic scroll', 1 ),
+    'Strength scroll': myItem( 'Strength scroll', 0x1F3C, 0x0000, 'magic scroll', 1 ),
+
+    # Third Circle
+    'Bless scroll': myItem( 'Bless scroll', 0x1F3D, 0x0000, 'magic scroll', 1 ),
+    'Fireball scroll': myItem( 'Fireball scroll', 0x1F3E, 0x0000, 'magic scroll', 1 ),
+    'Magic Lock scroll': myItem( 'Magic Lock scroll', 0x1F3F, 0x0000, 'magic scroll', 1 ),
+    'Poison scroll': myItem( 'Poison scroll', 0x1F40, 0x0000, 'magic scroll', 1 ),
+    'Telekinesis scroll': myItem( 'Telekinesis scroll', 0x1F41, 0x0000, 'magic scroll', 1 ),
+    'Teleport scroll': myItem( 'Teleport scroll', 0x1F42, 0x0000, 'magic scroll', 1 ),
+    'Unlock scroll': myItem( 'Unlock scroll', 0x1F43, 0x0000, 'magic scroll', 1 ),
+    'Wall of Stone scroll': myItem( 'Wall of Stone scroll', 0x1F44, 0x0000, 'magic scroll', 1 ),
+
+    # Fourth Circle
+    'Arch Cure scroll': myItem( 'Arch Cure scroll', 0x1F45, 0x0000, 'magic scroll', 1 ),
+    'Arch Protection scroll': myItem( 'Arch Protection scroll', 0x1F46, 0x0000, 'magic scroll', 1 ),
+    'Curse scroll': myItem( 'Curse scroll', 0x1F47, 0x0000, 'magic scroll', 1 ),
+    'Fire Field scroll': myItem( 'Fire Field scroll', 0x1F48, 0x0000, 'magic scroll', 1 ),
+    'Greater Heal scroll': myItem( 'Greater Heal scroll', 0x1F49, 0x0000, 'magic scroll', 1 ),
+    'Lightning scroll': myItem( 'Lightning scroll', 0x1F4A, 0x0000, 'magic scroll', 1 ),
+    'Mana Drain scroll': myItem( 'Mana Drain scroll', 0x1F4B, 0x0000, 'magic scroll', 1 ),
+    'Recall scroll': myItem( 'Recall scroll', 0x1F4C, 0x0000, 'magic scroll', 1 ),
+
+    # Fifth Circle
+    'Blade Spirits scroll': myItem( 'Blade Spirits scroll', 0x1F4D, 0x0000, 'magic scroll', 1 ),
+    'Dispel Field scroll': myItem( 'Dispel Field scroll', 0x1F4E, 0x0000, 'magic scroll', 1 ),
+    'Incognito scroll': myItem( 'Incognito scroll', 0x1F4F, 0x0000, 'magic scroll', 1 ),
+    'Magic Reflection scroll': myItem( 'Magic Reflection scroll', 0x1F50, 0x0000, 'magic scroll', 1 ),
+    'Mind Blast scroll': myItem( 'Mind Blast scroll', 0x1F51, 0x0000, 'magic scroll', 1 ),
+    'Paralyze scroll': myItem( 'Paralyze scroll', 0x1F52, 0x0000, 'magic scroll', 1 ),
+    'Poison Field scroll': myItem( 'Poison Field scroll', 0x1F53, 0x0000, 'magic scroll', 1 ),
+    'Summon Creature scroll': myItem( 'Summon Creature scroll', 0x1F54, 0x0000, 'magic scroll', 1 ),
+
+    # Sixth Circle
+    'Dispel scroll': myItem( 'Dispel scroll', 0x1F55, 0x0000, 'magic scroll', 1 ),
+    'Energy Bolt scroll': myItem( 'Energy Bolt scroll', 0x1F56, 0x0000, 'magic scroll', 1 ),
+    'Explosion scroll': myItem( 'Explosion scroll', 0x1F57, 0x0000, 'magic scroll', 1 ),
+    'Invisibility scroll': myItem( 'Invisibility scroll', 0x1F58, 0x0000, 'magic scroll', 1 ),
+    'Mark scroll': myItem( 'Mark scroll', 0x1F59, 0x0000, 'magic scroll', 1 ),
+    'Mass Curse scroll': myItem( 'Mass Curse scroll', 0x1F5A, 0x0000, 'magic scroll', 1 ),
+    'Paralyze Field scroll': myItem( 'Paralyze Field scroll', 0x1F5B, 0x0000, 'magic scroll', 1 ),
+    'Reveal scroll': myItem( 'Reveal scroll', 0x1F5C, 0x0000, 'magic scroll', 1 ),
+
+    # Seventh Circle
+    'Chain Lightning scroll': myItem( 'Chain Lightning scroll', 0x1F5D, 0x0000, 'magic scroll', 1 ),
+    'Energy Field scroll': myItem( 'Energy Field scroll', 0x1F5E, 0x0000, 'magic scroll', 1 ),
+    'Flamestrike scroll': myItem( 'Flamestrike scroll', 0x1F5F, 0x0000, 'magic scroll', 1 ),
+    'Gate Travel scroll': myItem( 'Gate Travel scroll', 0x1F60, 0x0000, 'magic scroll', 1 ),
+    'Mana Vampire scroll': myItem( 'Mana Vampire scroll', 0x1F61, 0x0000, 'magic scroll', 1 ),
+    'Mass Dispel scroll': myItem( 'Mass Dispel scroll', 0x1F62, 0x0000, 'magic scroll', 1 ),
+    'Meteor Swarm scroll': myItem( 'Meteor Swarm scroll', 0x1F63, 0x0000, 'magic scroll', 1 ),
+    'Polymorph scroll': myItem( 'Polymorph scroll', 0x1F64, 0x0000, 'magic scroll', 1 ),
+
+    # Eighth Circle
+    'Earthquake scroll': myItem( 'Earthquake scroll', 0x1F65, 0x0000, 'magic scroll', 1 ),
+    'Energy Vortex scroll': myItem( 'Energy Vortex scroll', 0x1F66, 0x0000, 'magic scroll', 1 ),
+    'Resurrection scroll': myItem( 'Resurrection scroll', 0x1F67, 0x0000, 'magic scroll', 1 ),
+    'Summon Air Elemental scroll': myItem( 'Summon Air Elemental scroll', 0x1F68, 0x0000, 'magic scroll', 1 ),
+    'Summon Daemon scroll': myItem( 'Summon Daemon scroll', 0x1F69, 0x0000, 'magic scroll', 1 ),
+    'Summon Earth Elemental scroll': myItem( 'Summon Earth Elemental scroll', 0x1F6A, 0x0000, 'magic scroll', 1 ),
+    'Summon Fire Elemental scroll': myItem( 'Summon Fire Elemental scroll', 0x1F6B, 0x0000, 'magic scroll', 1 ),
+    'Summon Water Elemental scroll': myItem( 'Summon Water Elemental scroll', 0x1F6C, 0x0000, 'magic scroll', 1 )
+}
