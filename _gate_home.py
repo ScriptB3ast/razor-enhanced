8dbeaf3b5b193481947ed8632942a29fc4bd415e
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:15:16 -0400

    Added macro to create and go through moongate to home. Useful for carrying heavy items to your house

---
+++ _gate_home.py
@@ -0,0 +1,17 @@
+from Scripts.glossary.items.moongates import FindMoongates
+if Player.Name == 'TheWarMage':
+    Items.UseItem( 0x42783F7F )
+elif Player.Name == 'TheWarPhysician':
+    Items.UseItem( 0x419880D0 )
+
+Gumps.WaitForGump( 1431013363, 2000 )
+Gumps.SendAction( 1431013363, 6 )
+
+moongates = FindMoongates( Items )
+while len( moongates ) == 0:
+    Misc.Pause( 20 )
+    moongates = FindMoongates( Items )
+
+Items.UseItem( moongates[ 0 ] )
+Gumps.WaitForGump( 3716879466, 2000 )
+Gumps.SendAction( 3716879466, 1 )
