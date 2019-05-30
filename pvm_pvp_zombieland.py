0dd092421315f321f2912ab6cfb2cb1826078490
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:27:42 -0400

    Added macro for attacking enemies in UO Forever's Zombie Land event

---
+++ pvm_pvp_zombieland.py
@@ -0,0 +1,35 @@
+from Scripts.glossary.enemies import GetEnemyNotorieties
+
+def AttackEnemy():
+    mobilesFilter = Mobiles.Filter()
+    mobilesFilter.RangeMax = 1
+    mobilesFilter.RangeMin = 1
+    mobilesFilter.Notorieties = GetEnemyNotorieties()
+    mob = Mobiles.ApplyFilter( mobilesFilter )
+    if len( mobile ) > 0:
+        mob = Mobiles.Select( mobile, 'Weakest' )
+    else:
+        return
+
+    mobPosition = mob.Position
+    playerPosition = Player.Position
+    if mobPosition.X == playerPosition.X and mobPosition.Y > playerPosition.Y:
+        Player.ChatSay( 0, '!1' )
+    elif mobPosition.X > playerPosition.X and mobPosition.Y > playerPosition.Y:
+        Player.ChatSay( 0, '!2' )
+    elif mobPosition.X > playerPosition.X and mobPosition.Y == playerPosition.Y:
+        Player.ChatSay( 0, '!3' )
+    elif mobPosition.X < playerPosition.X and mobPosition.Y > playerPosition.Y:
+        Player.ChatSay( 0, '!4' )
+    elif mobPosition.X > playerPosition.X and mobPosition.Y < playerPosition.Y:
+        Player.ChatSay( 0, '!6' )
+    elif mobPosition.X < playerPosition.X and mobPosition.Y == playerPosition.Y:
+        Player.ChatSay( 0, '!7' )
+    elif mobPosition.X < playerPosition.X and mobPosition.Y < playerPosition.Y:
+        Player.ChatSay( 0, '!8' )
+    elif mobPosition.X == playerPosition.X and mobPosition.Y < playerPosition.Y:
+        Player.ChatSay( 0, '!9' )
+
+while True:
+    AttackEnemy()
+    Misc.Pause( 1200 )
