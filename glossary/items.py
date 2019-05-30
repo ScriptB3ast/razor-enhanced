1ef5ce62ffc535257f209f40b2636b43758a4289
TheWarDoctor95 <tyler@dellajay.com>
2019-05-30 00:00:49 -0400

    Moved the statuette items into their own file

--- glossary/items.py
+++ glossary/items.py
@@ -17,14 +17,6 @@ class myItem:
         self.weight = weight
 
 items = {
-    ### Statuettes ###
-    'dread spider statuette': myItem( 'dread spider statuette', 0x25C4, 0x0000, 'statuette', 1 ),
-    'lich statuette': myItem( 'lich statuette', 0x25A4, 0x0000, 'statuette', 1 ),
-    'orc shaman statuette': myItem( 'orc shaman statuette', 0x25B1, 0x0000, 'statuette', 1 ),
-    'slime statuette': myItem( 'slime statuette', 0x20E8, 0x0000, 'statuette', 1 ),
-    'snow elemental statuette': myItem( 'snow elemental statuette', 0x25DC, 0x0000, 'statuette', 1 ),
-
-
     ### Tailoring Materials ###
     'cut cloth': myItem( 'cut cloth', 0x1767, 0x07C2, 'tailoring', 10 ),
     'pile of folded besotted cloth': myItem( 'pile of folded besotted cloth', 0x1766, 0x0000, 'tailoring', 1 ),
---
+++ glossary/items/statuettes.py
@@ -0,0 +1,9 @@
+from Scripts.utilities.items import myItem
+
+statuettes = {
+    'dread spider statuette': myItem( 'dread spider statuette', 0x25C4, 0x0000, 'statuette', 1 ),
+    'lich statuette': myItem( 'lich statuette', 0x25A4, 0x0000, 'statuette', 1 ),
+    'orc shaman statuette': myItem( 'orc shaman statuette', 0x25B1, 0x0000, 'statuette', 1 ),
+    'slime statuette': myItem( 'slime statuette', 0x20E8, 0x0000, 'statuette', 1 ),
+    'snow elemental statuette': myItem( 'snow elemental statuette', 0x25DC, 0x0000, 'statuette', 1 ),
+}
