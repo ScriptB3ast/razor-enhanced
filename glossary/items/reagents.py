70ff4b575cc24a81e776b097cd06963503bac187
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:59:37 -0400

    Moved the reagents into their own file

---
+++ glossary/items/reagents.py
@@ -0,0 +1,12 @@
+from Scripts.utilities.items import myItem
+
+reagents = {
+    'Black Pearl': myItem( 'Black Pearl', 0x0F7A, 0x0000, 'reagent', 0.1 ),
+    'Blood Moss': myItem( 'Blood Moss', 0x0F7B, 0x0000, 'reagent', 0.1 ),
+    'Garlic': myItem( 'Garlic', 0x0F84, 0x0000, 'reagent', 0.1 ),
+    'Ginseng': myItem( 'Ginseng', 0x0F85, 0x0000, 'reagent', 0.1 ),
+    'Mandrake Root': myItem( 'Mandrake Root', 0x0F86, 0x0000, 'reagent', 0.1 ),
+    'Nightshade': myItem( 'Nightshade', 0x0F88, 0x0000, 'reagent', 0.1 ),
+    'Spider\'s Silk': myItem( 'Spider\'s Silk', 0x0F8D, 0x0000, 'reagent', 0.1 ),
+    'Sulfurous Ash': myItem( 'Sulfurous Ash', 0x0F8C, 0x0000, 'reagent', 0.1 )
+}
