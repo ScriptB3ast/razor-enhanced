6cb997e46db9f13483bad4273b1a5fb3df352ece
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:47:45 -0400

    Moved the decoration items into their own file

---
+++ glossary/items/decorations.py
@@ -0,0 +1,30 @@
+from Scripts.utilities.items import myItem
+
+decorations = {
+    # Christmas
+    'box of snowdrops (east/west)': myItem( 'box of snowdrops', 0x3325, 0x0000, 'decoration', 1 ),
+    'box of snowdrops (north/south)': myItem( 'box of snowdrops', 0x3326, 0x0000, 'decoration', 1 ),
+    'broken ornament': myItem( 'broken ornament', 0x09B4, 0x0501, 'decoration', 1 ),
+    'christmas lights': myItem( 'christmas lights', 0x17CE, 0x0B93, 'decoration', 1 ),
+    'fancy holidy lamp': myItem( 'fancy holidy lamp', 0x4C51, 0x0785, 'decoration', 1 ),
+    'green candycane': myItem( 'green candycane', 0x2BDF, 0x0000, 'decoration', 1 ),
+    'holiday toy egg (red)': myItem( 'holiday toy egg (red)', 0x4CFD, 0x0026, 'decoration', 1 ),
+    'holiday toy egg (blue)': myItem( 'holiday toy egg (blue)', 0x4CFD, 0x0000, 'decoration', 1 ),
+    'holly jolly torch': myItem( 'holly jolly torch', 0x0F6B, 0x09E9, 'decoration', 1 ),
+    'marshmallow snowballs': myItem( 'marshmallow snowballs', 0x09EB, 0x0481, 'decoration', 1 ),
+    'old holiday sign (Santa\'s Workshop - Closed till next year)': myItem( 'old holiday sign (Santa\'s Workshop - Closed till next year)', 0x0BD9, 0x09F5, 'decoration', 1 ),
+    'red candycane': myItem( 'red candycane', 0x2BDE, 0x0000, 'decoration', 1 ),
+
+    # Dinner Ware
+    'ceramic mug': myItem( 'ceramic mug', 0x0995, 0x0000, 'decoration', 1 ),
+    'snow mug': myItem( 'snow mug', 0x0995, 0x0481, 'decoration', 1 ),
+
+    'bloody hourglass': myItem( 'bloody hourglass', 0x1810, 0x0A93, 'decoration', 1 ),
+    'dragon head': myItem( 'dragon head', 0x2235, 0x0000, 'decoration', 1 ),
+    'ettin fire brew': myItem( 'ettin fire brew', 0x495F, 0x081B, 'decoration', 1 ),
+    'hourglass': myItem( 'hourglass', 0x1810, 0x0000, 'decoration', 1 ),
+    'lamp stainglass': myItem( 'lamp stainglass', 0x4C51, 0x0000, 'decoration', 1 ),
+    'torch': myItem( 'torch', 0x0F6B, 0x0000, 'decoration', 1 ),
+    'tub of vegetables': myItem( 'tub of vegetables', 0x31CE, 0x0000, 'decoration', 1 ),
+    'UOForever\'s 6th Year': myItem( 'UOForever\'s 6th Year', 0x9BC8, 0x0AA1, 'decoration', 1 )
+}
