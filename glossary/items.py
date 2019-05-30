5d3037a6765ff29651222581bb576044410ddb40
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:43:56 -0400

    Moved the body part items into their own file

--- glossary/items.py
+++ glossary/items.py
@@ -17,19 +17,6 @@ class myItem:
         self.weight = weight
 
 items = {
-    ### Body Parts ###
-    'bone pile': myItem( 'bone pile', 0x1B10, 0x0000, 'body part', 10 ),
-    'bone shards': myItem( 'bone shards', 0x1B1A, 0x0000, 'body part', 1 ),
-    'bones of a temptress': myItem( 'bones of a temptress', 0x1B10, 0x0A4D, 'body part', 10 ),
-    'dragon bone shards': myItem( 'dragon bone shards', 0x1B1A, 0x09C2, 'body part', 1 ),
-    'dragon scales': myItem( 'dragon scales', 0x26B4, 0x0000, 'body part', 0.1 ),
-    'dragon scales (red)': myItem( 'dragon scales (red)', 0x26B4, 0x066D, 'body part', 0.1 ),
-    'dragon scales (blue)': myItem( 'dragon scales (blue)', 0x26B4, 0x08B0, 'body part', 0.1 ),
-    'dragon scales (golden)': myItem( 'dragon scales (golden)', 0x26B4, 0x08A8, 'body part', 0.1 ),
-    'intact dragon\'s heart': myItem( 'intact dragon\'s heart', 0x2808, 0x0A9E, 'body part', 1 ),
-    'skull': myItem( 'skull', 0x1AE1, 0x0000, 'body part', 1 ),
-
-
     ### Clothing ###
     # Anniversary
     '6th Year Forever Shirt': myItem( '6th Year Forever Shirt', 0x25EB, 0x0799, 'clothing', 1 ),
---
+++ glossary/items/bodyParts.py
@@ -0,0 +1,15 @@
+from Scripts.utilities.items import myItem
+
+bodyParts = {
+    ### Body Parts ###
+    'bone pile': myItem( 'bone pile', 0x1B10, 0x0000, 'body part', 10 ),
+    'bone shards': myItem( 'bone shards', 0x1B1A, 0x0000, 'body part', 1 ),
+    'bones of a temptress': myItem( 'bones of a temptress', 0x1B10, 0x0A4D, 'body part', 10 ),
+    'dragon bone shards': myItem( 'dragon bone shards', 0x1B1A, 0x09C2, 'body part', 1 ),
+    'dragon scales': myItem( 'dragon scales', 0x26B4, 0x0000, 'body part', 0.1 ),
+    'dragon scales (red)': myItem( 'dragon scales (red)', 0x26B4, 0x066D, 'body part', 0.1 ),
+    'dragon scales (blue)': myItem( 'dragon scales (blue)', 0x26B4, 0x08B0, 'body part', 0.1 ),
+    'dragon scales (golden)': myItem( 'dragon scales (golden)', 0x26B4, 0x08A8, 'body part', 0.1 ),
+    'intact dragon\'s heart': myItem( 'intact dragon\'s heart', 0x2808, 0x0A9E, 'body part', 1 ),
+    'skull': myItem( 'skull', 0x1AE1, 0x0000, 'body part', 1 )
+}
