ad7bf6643cec0bb6cab2fc5964090a963ca9b8cb
TheWarDoctor95 <tyler@dellajay.com>
2019-05-29 23:58:50 -0400

    Moved the ores into their own file

--- glossary/items.py
+++ glossary/items.py
@@ -17,19 +17,6 @@ class myItem:
         self.weight = weight
 
 items = {
-    ### Ores ###
-    # Weights are set to None since the weight varies depending on the player's mining skill
-    'agapite ore': myItem( 'agapite ore', 0x19B9, 0x097E, 'ore', None ),
-    'bronze ore': myItem( 'bronze ore', 0x19B9, 0x06D8, 'ore', None ),
-    'copper ore': myItem( 'copper ore', 0x19B9, 0x045F, 'ore', None ),
-    'dull copper ore': myItem( 'dull copper ore', 0x19B9, 0x0415, 'ore', None ),
-    'golden ore': myItem( 'golden ore', 0x19B9, 0x06B7, 'ore', None ),
-    'iron ore': myItem( 'iron ore', 0x19B9, 0x0000, 'ore', None ),
-    'shadow iron ore': myItem( 'shadow iron ore', 0x19B9, 0x0455, 'ore', None ),
-    'verite ore': myItem( 'verite ore', 0x19B9, 0x07D2, 'ore', None ),
-    'valorite ore': myItem( 'valorite ore', 0x19B9, 0x0544, 'ore', None ),
-
-
     ### Potions ###
     'keg of greater strength potions': myItem( 'keg of greater strength potions', 0x1940, 0x03B8, 'potion', 1 ),
     'bottle of toxic poison': myItem( 'bottle of toxic poison', 0x0EFB, 0x0785, 'potion', 1 ),
---
+++ glossary/items/ores.py
@@ -0,0 +1,14 @@
+from Scripts.utilities.items import myItem
+
+ores = {
+    # Weights are set to None since the weight varies depending on the player's mining skill
+    'agapite ore': myItem( 'agapite ore', 0x19B9, 0x097E, 'ore', None ),
+    'bronze ore': myItem( 'bronze ore', 0x19B9, 0x06D8, 'ore', None ),
+    'copper ore': myItem( 'copper ore', 0x19B9, 0x045F, 'ore', None ),
+    'dull copper ore': myItem( 'dull copper ore', 0x19B9, 0x0415, 'ore', None ),
+    'golden ore': myItem( 'golden ore', 0x19B9, 0x06B7, 'ore', None ),
+    'iron ore': myItem( 'iron ore', 0x19B9, 0x0000, 'ore', None ),
+    'shadow iron ore': myItem( 'shadow iron ore', 0x19B9, 0x0455, 'ore', None ),
+    'verite ore': myItem( 'verite ore', 0x19B9, 0x07D2, 'ore', None ),
+    'valorite ore': myItem( 'valorite ore', 0x19B9, 0x0544, 'ore', None )
+}
