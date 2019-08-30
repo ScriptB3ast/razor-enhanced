9cabdd45ce7951d11b6b033467ae6999a7815486
TheWarDoctor95 <tyler@dellajay.com>
2019-08-30 15:39:53 -0400

    Changed flute to bamboo flute, since that's how it's labeled in game

--- glossary/items/instruments.py
+++ glossary/items/instruments.py
@@ -1,8 +1,8 @@
 from Scripts.utilities.items import myItem, FindItem
 
 instruments = {
+    'bamboo flute': myItem( 'bamboo flute', 0x2805, 0x0000, 'instrument', None ),
     'drum': myItem( 'drum', 0x0E9C, 0x0000, 'instrument', None ),
-    'flute': myItem( 'flute', 0x2805, 0x0000, 'instrument', None ),
     'lute': myItem( 'lute', 0x0EB3, 0x0000, 'instrument', None ),
 
     # Harps
