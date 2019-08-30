ed78101396f430d0b50dcff15d74c6c9c8f4ca86
TheWarDoctor95 <tyler@dellajay.com>
2019-08-30 14:01:24 -0400

    Updated to use the nearest moongate

--- items_useMoongate.py
+++ items_useMoongate.py
@@ -2,7 +2,8 @@ from Scripts.glossary.items.moongates import FindMoongates
 
 moongates = FindMoongates( Items )
 if len( moongates ) > 0:
-    Items.UseItem( moongates[ 0 ] )
+    moongate = Items.ApplyFilter( moongates, 'Nearest' )
+    Items.UseItem( moongate )
 
     Gumps.WaitForGump( 3716879466, 2000 )
     Gumps.SendAction( 3716879466, 1 )
