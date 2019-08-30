5463763209f514dbaf3cbb5eb8a7309854d09529
TheWarDoctor95 <tyler@dellajay.com>
2019-08-30 15:39:31 -0400

    Reformated and reorganized. Also added some missing items that could be crafted from the Tailoring gump

--- glossary/items/clothing.py
+++ glossary/items/clothing.py
@@ -2,37 +2,41 @@ from Scripts.utilities.items import myItem
 
 clothing = {
     # Anniversary
-    '6th Year Forever Shirt': myItem( '6th Year Forever Shirt', 0x25EB, 0x0799, 'clothing', 1 ),
-    'UOForever\'s 6th Year Anniversary': myItem( 'UOForever\'s 6th Year Anniversary', 0x2B6E, 0x0000, 'decoration', 1 ),
+    '6th Year Forever Shirt': myItem(
+        name = '6th Year Forever Shirt',
+        itemID = 0x25EB,
+        color = 0x0799,
+        category = 'clothing',
+        weight = 1
+    ),
+    'UOForever\'s 6th Year Anniversary': myItem(
+        name = 'UOForever\'s 6th Year Anniversary',
+        itemID = 0x2B6E,
+        color = 0x0000,
+        category = 'decoration',
+        weight = 1
+    ),
 
     # Christmas
-    'jingle bells necklace': myItem( 'jingle bells necklace', 0x4D0A, 0x0501, 'decoration', 1 ),
+    'jingle bells necklace': myItem(
+        name = 'jingle bells necklace',
+        itemID = 0x4D0A,
+        color = 0x0501,
+        category = 'decoration',
+        weight = 1
+    ),
 
+    # Hats
+    'skullcap': myItem(
+        name = 'skullcap',
+        itemID = 0x1544,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
     'bandana': myItem(
         name = 'bandana',
         itemID = 0x1540,
-        color = 0x0000,
-        category = 'clothing',
-        weight = 1
-    ),
-    'bonnet': myItem(
-        name = 'bonnet',
-        itemID = 0x1719,
-        color = 0x0000,
-        category = 'clothing',
-        weight = 1
-    ),
-    'cap': myItem(
-        name = 'cap',
-        itemID = 0x1715,
-        color = 0x0000,
-        category = 'clothing',
-        weight = 1
-    ),
-    'checkered shirt': myItem( 'checkered shirt', 0x25EB, 0x0000, 'clothing', 1 ),
-    'feathered hat': myItem(
-        name = 'feathered hat',
-        itemID = 0x171A,
         color = 0x0000,
         category = 'clothing',
         weight = 1
@@ -44,26 +48,16 @@ clothing = {
         category = 'clothing',
         weight = 1
     ),
-    'flower garland': myItem(
-        name = 'flower garland',
-        itemID = 0x2306,
+    'cap': myItem(
+        name = 'cap',
+        itemID = 0x1715,
         color = 0x0000,
         category = 'clothing',
         weight = 1
     ),
-    'full apron': myItem( 'full apron', 0x153D, 0x0000, 'clothing', 4 ),
-    'gargish necklace': myItem( 'gargish necklace', 0x4D0A, 0x0000, 'clothing', 1 ),
-    'jester hat': myItem(
-        name = 'jester hat',
-        itemID = 0x171C,
-        color = 0x0000,
-        category = 'clothing',
-        weight = 1
-    ),
-    'Kiss the Cook apron': myItem( 'Kiss the Cook apron', 0x153D, 0x0844, 'clothing', 4 ),
-    'skullcap': myItem(
-        name = 'skullcap',
-        itemID = 0x1544,
+    'wide-brim hat': myItem(
+        name = 'wide-brim hat',
+        itemID = 0x1714,
         color = 0x0000,
         category = 'clothing',
         weight = 1
@@ -82,6 +76,27 @@ clothing = {
         category = 'clothing',
         weight = 1
     ),
+    'wizard\'s hat': myItem(
+        name = 'wizard\'s hat',
+        itemID = 0x1718,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'bonnet': myItem(
+        name = 'bonnet',
+        itemID = 0x1719,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'feathered hat': myItem(
+        name = 'feathered hat',
+        itemID = 0x171A,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
     'tricorne hat': myItem(
         name = 'tricorne hat',
         itemID = 0x171B,
@@ -89,19 +104,211 @@ clothing = {
         category = 'clothing',
         weight = 1
     ),
-    'wide-brim hat': myItem(
-        name = 'wide-brim hat',
-        itemID = 0x1714,
+    'jester hat': myItem(
+        name = 'jester hat',
+        itemID = 0x171C,
         color = 0x0000,
         category = 'clothing',
         weight = 1
     ),
-    'wizard\'s hat': myItem(
-        name = 'wizard\'s hat',
-        itemID = 0x1718,
+
+    # Shirts
+    'doublet': myItem(
+        name = 'doublet',
+        itemID = 0x1F7B,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'shirt': myItem(
+        name = 'shirt',
+        itemID = 0x1517,
         color = 0x0000,
         category = 'clothing',
         weight = 1
+    ),
+    'fancy shirt': myItem(
+        name = 'fancy shirt',
+        itemID = 0x1EFD,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'tunic': myItem(
+        name = 'tunic',
+        itemID = 0x1FA1,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 5
+    ),
+    'surcoat': myItem(
+        name = 'surcoat',
+        itemID = 0x1FFD,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 6
+    ),
+    'plain dress': myItem(
+        name = 'plain dress',
+        itemID = 0x1F01,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'fancy dress': myItem(
+        name = 'fancy dress',
+        itemID = 0x1F00,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 3
+    ),
+    'cloak': myItem(
+        name = 'cloak',
+        itemID = 0x1515,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 5
+    ),
+    'robe': myItem(
+        name = 'robe',
+        itemID = 0x1F03,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 3
+    ),
+    'jester suit': myItem(
+        name = 'jester suit',
+        itemID = 0x1F9F,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 4
+    ),
+
+    # Pants
+    'short pants': myItem(
+        name = 'short pants',
+        itemID = 0x152E,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'long pants': myItem(
+        name = 'long pants',
+        itemID = 0x1539,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'kilt': myItem(
+        name = 'kilt',
+        itemID = 0x1537,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'skirt': myItem(
+        name = 'skirt',
+        itemID = 0x1516,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+
+    # Miscellaneous Tailoring
+    'body sash': myItem(
+        name = 'body sash',
+        itemID = 0x1541,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'half apron': myItem(
+        name = 'half apron',
+        itemID = 0x153B,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'full apron': myItem(
+        name = 'full apron',
+        itemID = 0x153D,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 4
+    ),
+    'oil cloth': myItem(
+        name = 'oil cloth',
+        itemID = 0x175D,
+        color = 0x0363,
+        category = 'clothing',
+        weight = 2
+    ),
+
+    # Footwear
+    'sandals': myItem(
+        name = 'sandals',
+        itemID = 0x170D,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'shoes': myItem(
+        name = 'shoes',
+        itemID = 0x170F,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 2
+    ),
+    'boots': myItem(
+        name = 'boots',
+        itemID = 0x170B,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 3
+    ),
+    'thigh boots': myItem(
+        name = 'thigh boots',
+        itemID = 0x1711,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 4
+    ),
+
+
+    'checkered shirt': myItem(
+        name = 'checkered shirt',
+        itemID = 0x25EB,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'flower garland': myItem(
+        name = 'flower garland',
+        itemID = 0x2306,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'full apron': myItem(
+        name = 'full apron',
+        itemID = 0x153D,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 4
+    ),
+    'gargish necklace': myItem(
+        name = 'gargish necklace',
+        itemID = 0x4D0A,
+        color = 0x0000,
+        category = 'clothing',
+        weight = 1
+    ),
+    'Kiss the Cook apron': myItem(
+        name = 'Kiss the Cook apron',
+        itemID = 0x153D,
+        color = 0x0844,
+        category = 'clothing',
+        weight = 4
     ),
 }
 
