1a7b0e64e8d787f9fb60ff39339c1637575d472b
TheWarDoctor95 <tyler@dellajay.com>
2019-10-19 18:52:11 -0400

    Updated to attack correctly and to attack the same mob until it is dead

--- pvm_pvp_zombieland.py
+++ pvm_pvp_zombieland.py
@@ -4,32 +4,35 @@ def AttackEnemy():
     mobilesFilter = Mobiles.Filter()
     mobilesFilter.RangeMax = 1
     mobilesFilter.RangeMin = 1
+    mobilesFilter.Friend = 0
     mobilesFilter.Notorieties = GetEnemyNotorieties()
-    mob = Mobiles.ApplyFilter( mobilesFilter )
+    mobile = Mobiles.ApplyFilter( mobilesFilter )
     if len( mobile ) > 0:
-        mob = Mobiles.Select( mobile, 'Weakest' )
+        mobile = Mobiles.Select( mobile, 'Weakest' )
     else:
         return
 
-    mobPosition = mob.Position
-    playerPosition = Player.Position
-    if mobPosition.X == playerPosition.X and mobPosition.Y > playerPosition.Y:
-        Player.ChatSay( 0, '!1' )
-    elif mobPosition.X > playerPosition.X and mobPosition.Y > playerPosition.Y:
-        Player.ChatSay( 0, '!2' )
-    elif mobPosition.X > playerPosition.X and mobPosition.Y == playerPosition.Y:
-        Player.ChatSay( 0, '!3' )
-    elif mobPosition.X < playerPosition.X and mobPosition.Y > playerPosition.Y:
-        Player.ChatSay( 0, '!4' )
-    elif mobPosition.X > playerPosition.X and mobPosition.Y < playerPosition.Y:
-        Player.ChatSay( 0, '!6' )
-    elif mobPosition.X < playerPosition.X and mobPosition.Y == playerPosition.Y:
-        Player.ChatSay( 0, '!7' )
-    elif mobPosition.X < playerPosition.X and mobPosition.Y < playerPosition.Y:
-        Player.ChatSay( 0, '!8' )
-    elif mobPosition.X == playerPosition.X and mobPosition.Y < playerPosition.Y:
-        Player.ChatSay( 0, '!9' )
+    while Mobiles.FindBySerial( mobile.Serial ) != None and mobile.DistanceTo( Mobiles.FindBySerial( Player.Serial ) ) < 2:
+        mobPosition = mobile.Position
+        playerPosition = Player.Position
+        if mobPosition.X == playerPosition.X and mobPosition.Y > playerPosition.Y:
+            Player.ChatSay( 0, '!1' )
+        elif mobPosition.X > playerPosition.X and mobPosition.Y > playerPosition.Y:
+            Player.ChatSay( 0, '!2' )
+        elif mobPosition.X > playerPosition.X and mobPosition.Y == playerPosition.Y:
+            Player.ChatSay( 0, '!3' )
+        elif mobPosition.X < playerPosition.X and mobPosition.Y > playerPosition.Y:
+            Player.ChatSay( 0, '!4' )
+        elif mobPosition.X > playerPosition.X and mobPosition.Y < playerPosition.Y:
+            Player.ChatSay( 0, '!6' )
+        elif mobPosition.X < playerPosition.X and mobPosition.Y == playerPosition.Y:
+            Player.ChatSay( 0, '!7' )
+        elif mobPosition.X < playerPosition.X and mobPosition.Y < playerPosition.Y:
+            Player.ChatSay( 0, '!8' )
+        elif mobPosition.X == playerPosition.X and mobPosition.Y < playerPosition.Y:
+            Player.ChatSay( 0, '!9' )
+        Misc.Pause( 1200 )
 
 while True:
     AttackEnemy()
-    Misc.Pause( 1200 )
+    Misc.Pause( 200 )
